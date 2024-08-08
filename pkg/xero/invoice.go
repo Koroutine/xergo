fc1ba8a235bcb4fa4c19df496d3834b8ef928b95
Michael J. Roberts <michael.r@koroutine.tech>
2024-08-08 17:46:35 +0100

    feat: add (c *XeroClient) GetInvoices
    feat: add (c *XeroClient) GetInvoices

---
+++ pkg/xero/invoice.go
@@ -0,0 +1,41 @@
+package xero
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type Invoice struct {
+	InvoiceID     string `json:"InvoiceID"`
+	InvoiceNumber string `json:"InvoiceNumber"`
+}
+
+type InvoicesResponse struct {
+	Invoices []Invoice `json:"Invoices"`
+}
+
+func (c *XeroClient) GetInvoices() (*InvoicesResponse, error) {
+	response, err := c.client.Get("https://api.xero.com/api.xro/2.0/Invoices")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var invoicesResponse InvoicesResponse
+
+	err = json.Unmarshal(body, &invoicesResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &invoicesResponse, nil
+}
