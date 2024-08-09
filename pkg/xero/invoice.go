41b33d168dbcb2eabf2b1404bb424e07f4ac1d98
michealroberts <michael@observerly.com>
2024-08-09 15:50:34 +0100

    feat: add (c *XeroClient) GetInvoiceByID
    feat: add (c *XeroClient) GetInvoiceByID

--- pkg/xero/invoice.go
+++ pkg/xero/invoice.go
@@ -2,6 +2,7 @@ package xero
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 )
 
@@ -40,4 +41,40 @@ func (c *XeroClient) GetInvoices() (*InvoicesResponse, error) {
 	}
 
 	return &invoicesResponse, nil
+}
+
+func (c *XeroClient) GetInvoiceByID(invoiceID string) (*Invoice, error) {
+	req := c.SetupBaseRequest(GET, fmt.Sprintf("/api.xro/2.0/Invoices/%s", invoiceID))
+
+	response, err := c.client.Do(&req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("no invoice found with ID: %s", invoiceID)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var invoicesResponse InvoicesResponse
+
+	err = json.Unmarshal(body, &invoicesResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(invoicesResponse.Invoices) == 0 {
+		return nil, fmt.Errorf("no invoice found with ID: %s", invoiceID)
+	}
+
+	return &invoicesResponse.Invoices[0], nil
 }
