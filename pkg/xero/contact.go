376bc30e036341c19f053995a3fc9c9c27f2d32e
Michael J. Roberts <michael.r@koroutine.tech>
2024-08-09 16:46:02 +0100

    feat: add (c *XeroClient) GetContactById
    feat: add (c *XeroClient) GetContactById

--- pkg/xero/contact.go
+++ pkg/xero/contact.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
 )
 
 type Contact struct {
@@ -48,4 +49,42 @@ func (c *XeroClient) GetContacts() (*ContactsResponse, error) {
 	}
 
 	return &contactsResponse, nil
+}
+
+func (c *XeroClient) GetContactById(contactID string) (*Contact, error) {
+	req := c.SetupBaseRequest(http.MethodGet, fmt.Sprintf("/api.xro/2.0/Contacts/%s", contactID))
+
+	response, err := c.client.Do(&req)
+
+	if err != nil {
+		return nil, fmt.Errorf("error making API call: %w", err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("could not get contact, unexpected status code: %d", response.StatusCode)
+	}
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var contactResponse struct {
+		Contacts []Contact `json:"Contacts"`
+	}
+
+	err = json.Unmarshal(body, &contactResponse)
+
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling response: %w", err)
+	}
+
+	if len(contactResponse.Contacts) == 0 {
+		return nil, fmt.Errorf("no contact found with ID %s", contactID)
+	}
+
+	return &contactResponse.Contacts[0], nil
 }
