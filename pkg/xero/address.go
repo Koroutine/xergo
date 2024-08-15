452323811df525f8d44436e04ca0be710d2c009d
Michael J. Roberts <michael.r@koroutine.tech>
2024-08-15 16:29:11 +0100

    refactor: amend Address type
    refactor: amend Address type

--- pkg/xero/address.go
+++ pkg/xero/address.go
@@ -1,9 +1,13 @@
 package xero
 
 type Address struct {
-	AddressType string `json:"AddressType"`
-	City        string `json:"City"`
-	Region      string `json:"Region"`
-	PostalCode  string `json:"PostalCode"`
-	Country     string `json:"Country"`
+	AddressType  string `json:"AddressType,omitempty"`
+	AddressLine1 string `json:"AddressLine1,omitempty"`
+	AddressLine2 string `json:"AddressLine2,omitempty"`
+	AddressLine3 string `json:"AddressLine3,omitempty"`
+	AddressLine4 string `json:"AddressLine4,omitempty"`
+	City         string `json:"City,omitempty"`
+	Region       string `json:"Region,omitempty"`
+	PostalCode   string `json:"PostalCode,omitempty"`
+	Country      string `json:"Country,omitempty"`
 }
